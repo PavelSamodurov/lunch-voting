849a09c0393a6709a096b6cb41d1e760d51dce9e
Pavel Samodurov <ps07t447@gmail.com>
2019-11-24 19:40:13 +0300

    create-db and insert-data

---
+++ src/main/resources/db/create-db.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS restaurant;
+DROP TABLE IF EXISTS lunch_menu;
+DROP TABLE IF EXISTS dish;
+DROP SEQUENCE IF EXISTS global_seq;
+
+CREATE SEQUENCE global_seq START WITH 100000;
+
+CREATE TABLE restaurant
+(
+  id               INTEGER DEFAULT global_seq.nextval PRIMARY KEY,
+  name             VARCHAR(255)            NOT NULL
+);
+
+CREATE TABLE lunch_menu
+(
+  id               INTEGER DEFAULT global_seq.nextval PRIMARY KEY,
+  date             DATE               NOT NULL,
+  restaurant_id    INTEGER                 NOT NULL,
+  FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE
+);
+
+CREATE TABLE dish
+(
+  id               INTEGER DEFAULT global_seq.nextval PRIMARY KEY,
+  name             VARCHAR(255)            NOT NULL,
+  price            BIGINT                  NOT NULL,
+  lunch_menu_id    INTEGER                 NOT NULL,
+  FOREIGN KEY (lunch_menu_id) REFERENCES lunch_menu (id) ON DELETE CASCADE
+);
