2868e74976e71844be83eca72d4c270ff33079a8
Pavel Samodurov <ps07t447@gmail.com>
2019-12-28 10:51:59 +0300

    rest test jackson

--- src/main/java/com/lunchvoting/model/AbstractBaseEntity.java
+++ src/main/java/com/lunchvoting/model/AbstractBaseEntity.java
@@ -1,17 +1,25 @@
 package com.lunchvoting.model;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import org.hibernate.Hibernate;
+import org.springframework.data.domain.Persistable;
+
 import javax.persistence.*;
+
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 
 @MappedSuperclass
 // http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
 @Access(AccessType.FIELD)
-public class AbstractBaseEntity {
+@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, isGetterVisibility = NONE, setterVisibility = NONE)
+public class AbstractBaseEntity implements Persistable<Integer> {
     public static final int START_SEQ = 100_000;
 
     @Id
     @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
-    protected int id;
+    protected Integer id;
 
     protected AbstractBaseEntity() {
     }
@@ -20,11 +28,38 @@ public class AbstractBaseEntity {
         this.id = id;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ":" + id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !getClass().equals(Hibernate.getClass(o))) {
+            return false;
+        }
+        AbstractBaseEntity that = (AbstractBaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
     }
 }
